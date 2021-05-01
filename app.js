a92cb82e5c648af3b208245c61b49b2e0705eeb9
Duncan Alexander <hypo@hypothete.com>
2021-05-01 09:49:46 -0700

    testing pulling in cameras

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import * as THREE from './vendor/three.module.js';
+import * as THREE from './node_modules/three/build/three.module.js';
 
 const scene = new THREE.Scene();
 let width = window.innerWidth;
@@ -7,10 +7,15 @@ const camera = new THREE.PerspectiveCamera(45, width/height, 0.1, 100);
 const renderer = new THREE.WebGLRenderer();
 let fragmentShader, vertexShader;
 
+let poses;
+const worldAxis = new THREE.AxisHelper(5);
+
 renderer.setSize(width, height);
 document.body.appendChild(renderer.domElement);
-camera.position.z = 2;
-scene.add(camera);
+camera.position.set(20, 20, -10);
+camera.lookAt(new THREE.Vector3());
+
+scene.add(camera, worldAxis);
 
 window.addEventListener('resize', () => {
   width = window.innerWidth;
@@ -29,6 +34,7 @@ function animate() {
 }
 
 async function loadScene() {
+  await loadImageData();
   await loadShaders();
   animate();
 }
@@ -37,4 +43,34 @@ async function loadShaders() {
   vertexShader = await fetch('./vertex.glsl').then(res => res.text());
   fragmentShader = await fetch('./fragment.glsl').then(res => res.text());
   console.log('Loaded shaders');
+}
+
+async function loadImageData() {
+  const rawText = await fetch('./data/images.txt').then(res => res.text());
+  poses = rawText
+    .split(`\r\n`)
+    .filter(line => {
+      return line.endsWith('jpg');
+    })
+    .map(line => {
+      const fields = line.split(' ');
+      return {
+        imageId: Number(fields[0]) - 1,
+        quaternion: new THREE.Quaternion(...fields.slice(1, 4)).normalize(),
+        translation: new THREE.Vector3(...fields.slice(5, 7)),
+      };
+    });
+  console.log(poses);
+
+  // rotate on X axis to match THREE handedness
+  const adjustment = new THREE.Quaternion();
+  adjustment.setFromAxisAngle(new THREE.Vector3(1, 0, 0), -Math.PI/2);
+
+  poses.forEach(pose => {
+    const axis = new THREE.AxisHelper();
+    axis.position.copy(pose.translation);
+    axis.applyQuaternion(pose.quaternion);
+    axis.quaternion.multiply(adjustment);
+    scene.add(axis);
+  });
 }
